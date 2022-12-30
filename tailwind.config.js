b2ebd778a432d4ab363a2fc2cf91a898d67333ad
Kuda <kudam775@gmail.com>
2022-12-31 01:03:38 +0200

    custom configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./css/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+      },
+    },
   },
   plugins: [],
 }
